c9dde3c0f4ffeb57be90a6dd31a91c3cf629577a
Brian Glover <brian@access9.net>
2014-12-18 11:11:32 -0600

    Fixed require_once for composer.

--- src/Access9/Console/Application.php
+++ src/Access9/Console/Application.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Input\InputDefinition;
 class Application extends sfApplication
 {
     const APP_NAME   = 'DbTableDump';
-    const APP_VERION = '0.6.1';
+    const APP_VERION = '0.6.2';
 
     /**
      * @var \Doctrine\DBAL\Connection

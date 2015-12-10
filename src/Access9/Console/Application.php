ef5e7c477af8897534d0c07357bb4514ca46a8a4
Brian Glover <brian@access9.net>
2015-12-09 21:43:04 -0600

    Updated version string.

--- src/Access9/Console/Application.php
+++ src/Access9/Console/Application.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Input\InputDefinition;
 class Application extends sfApplication
 {
     const APP_NAME   = 'DbTableDump';
-    const APP_VERSION = '0.8.3';
+    const APP_VERSION = '0.8.5.1';
 
     /**
      * @var \Doctrine\DBAL\Connection

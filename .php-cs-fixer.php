9a4e2256e0e4115a6fd338d6b499a48b15d539f4
Brian Glover <brian@access9.net>
2024-07-20 11:23:17 -0500

    Updated php-cs-fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -7,30 +7,32 @@ $finder = PhpCsFixer\Finder::create()
     ->in('tests/');
 
 return (new PhpCsFixer\Config())->setRules([
-    '@PSR2'                                 => true,
-    'cast_spaces'                           => ['space' => 'single'],
-    'concat_space'                          => false,
-    'array_syntax'                          => ['syntax' => 'short'],
-    'no_trailing_comma_in_singleline_array' => true,
-    'trim_array_spaces'                     => true,
-    'ordered_imports'                       => true,
-    'phpdoc_separation'                     => false,
-    'phpdoc_annotation_without_dot'         => false,
-    'phpdoc_summary'                        => false,
-    'phpdoc_indent'                         => true,
-    'phpdoc_order'                          => true,
-    'phpdoc_types'                          => true,
-    'phpdoc_trim'                           => true,
-    'increment_style'                       => false,
-    'no_blank_lines_before_namespace'       => true,
-    'no_leading_import_slash'               => true,
-    'blank_line_before_statement'           => ['statements' => ['return']],
-    'binary_operator_spaces'                => [
+    '@PSR2'                           => true,
+    'cast_spaces'                     => ['space' => 'single'],
+    'concat_space'                    => false,
+    'array_syntax'                    => ['syntax' => 'short'],
+    'no_trailing_comma_in_singleline' => [
+        'elements' => ['arguments', 'array_destructuring', 'array', 'group_import'],
+    ],
+    'trim_array_spaces'               => true,
+    'ordered_imports'                 => true,
+    'phpdoc_separation'               => false,
+    'phpdoc_annotation_without_dot'   => false,
+    'phpdoc_summary'                  => false,
+    'phpdoc_indent'                   => true,
+    'phpdoc_order'                    => true,
+    'phpdoc_types'                    => true,
+    'phpdoc_trim'                     => true,
+    'increment_style'                 => false,
+    'blank_lines_before_namespace'    => ['min_line_breaks' => 1, 'max_line_breaks' => 1],
+    'no_leading_import_slash'         => true,
+    'blank_line_before_statement'     => ['statements' => ['return']],
+    'binary_operator_spaces'          => [
         'operators' => [
-            '='  => 'align_single_space',
-            '=>' => 'align_single_space',
+            '='   => 'align_single_space',
+            '=>'  => 'align_single_space',
+            '??=' => 'align_single_space',
         ],
     ],
-    'full_opening_tag'                      => true,
-])
-->setFinder($finder);
+    'full_opening_tag'                => true,
+])->setFinder($finder);
